1ec1c03f113d97dee4b25997764f2d02e0e4f69e
jintkim-robo <jintkim@roboco-op.org>
2025-12-15 23:19:46 +0100

    Create start.js

---
+++ api/start.js
@@ -0,0 +1,24 @@
+export default async function handler(req, res) {
+  try {
+    const response = await fetch("https://tavusapi.com/v2/conversations", {
+      method: "POST",
+      headers: {
+        "Authorization": process.env.TAVUS_API_KEY,
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        persona_id: process.env.TAVUS_PERSONA_ID
+      })
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      return res.status(response.status).json(data);
+    }
+
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+}
